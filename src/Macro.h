244b68db367da6855e3748a97956d6422213a32c
Tom Daley <tomdaley92@gmail.com>
2017-10-14 13:17:17 -0700

    Add macro class files. Recompile.

---
+++ src/Macro.h
@@ -0,0 +1,30 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+/*
+
+condition with possible nested conditions
+
+- write a class that can parse a mapping-defition string into an easy to test condition.
+- ideally a 1 liner
+
+& operator -> button_butmask & buttons == button_butmask
+| operator -> button_butmask & buttons
+! operator -> button_butmask & buttons != button_butmask
+
+*/
+
+/* forward declaration */
+typedef struct CONDITION CONDITION;
+
+typedef struct OPERAND {
+    unsigned short button_mask;
+    CONDITION *condition; // pointer to another condition (if null, then the button is valid)
+} OPERAND;
+
+typedef struct CONDITION {
+    char op; // operator is a c++ keyword unfortunatley
+    OPERAND operand;
+} CONDITION;
+
+#endif
